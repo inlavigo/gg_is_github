4022bcacfa7b3578d077a55ee8b44d4f2d081b95
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-03-08 16:57:37 +0100

    "Initial boylerplate"

---
+++ README.md
@@ -0,0 +1,3 @@
+# gg_is_github
+
+Provides a method isGitHubAction which returns true if the currenct directory is running as part of a GitHub action.
---
+++ test/vscode/launch_json_test.dart
@@ -0,0 +1,73 @@
+// @license
+// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:test/test.dart';
+
+void main() {
+  // #########################################################################
+  group('.vscode/launch.json', () {
+    test('pathes in launch.json', () {
+      const String launchJsonPath = '.vscode/launch.json';
+      final String launchJson = File(launchJsonPath)
+          .readAsStringSync()
+          .replaceAll(RegExp(r'//.*'), '');
+
+      final parsedLaunchJson = jsonDecode(launchJson) as Map<String, dynamic>;
+
+      // Ensure there is a configuration for executing bin/gg_is_github.dart
+      final configurations =
+          parsedLaunchJson['configurations'] as List<dynamic>;
+
+      final ggIsGithub = configurations.firstWhere(
+        (dynamic configuration) =>
+            configuration['name'].toString() == 'gg_is_github.dart',
+      );
+
+      expect(
+        ggIsGithub,
+        isNotNull,
+        reason: 'Item for executing bin/gg_is_github.dart not found',
+      );
+
+      expect(ggIsGithub['name'], 'gg_is_github.dart');
+      expect(ggIsGithub['type'], 'dart');
+      expect(ggIsGithub['request'], 'launch');
+
+      // Ensure there is a coniguration for executing the current file
+      final currentFile = parsedLaunchJson['configurations'].firstWhere(
+        (dynamic configuration) => configuration['name'] == 'Current File',
+      );
+      expect(
+        currentFile,
+        isNotNull,
+        reason: 'Item for executing current file not found',
+      );
+      expect(currentFile['type'], 'dart');
+      expect(currentFile['request'], 'launch');
+      expect(currentFile['program'], r'${file}');
+
+      // Test other configurations
+      for (final configuration
+          in parsedLaunchJson['configurations'] as Iterable) {
+        final program = configuration['program'].toString();
+        expect(program, isNotNull);
+        expect(program, isNotEmpty);
+        if (program.contains('{workspaceFolder}')) {
+          final resolved = program.replaceAll(r'${workspaceFolder}', '.');
+          final file = File(resolved);
+          expect(file.existsSync(), isTrue);
+          final content = file.readAsStringSync();
+          if (file.path.endsWith('.dart')) {
+            expect(content, contains('main('));
+          }
+        }
+      }
+    });
+  });
+}
